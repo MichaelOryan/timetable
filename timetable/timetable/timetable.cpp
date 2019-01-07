282917e0aed0603ee78f2278a6e96402f6fe586c
trumpler <40741271+trumpler@users.noreply.github.com>
2019-01-07 19:19:18 +0800

    Update timetable.cpp

--- timetable/timetable/timetable.cpp
+++ timetable/timetable/timetable.cpp
@@ -6,11 +6,9 @@
 #include <Windows.h>
 #include <Mmsystem.h>
 #include <string.h>
-#include <ctime>
 #include <chrono>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <string>
 #include <vector>
 
@@ -18,30 +16,12 @@
 
 using namespace std;
 
-void displayTimeTable(string x,int y) {
-	cout << "topic: "<< x << " | " << "Duration: " << y << " min" << endl;
+void displayTimeTable(string x, int y) {
+	cout << "topic: " << x << " | " << "Duration: " << y << " min" << endl;
 }
 
 //duration included
-int inputChecking() {                                      
-	int input;
-
-	do {
-		cout << "Duration(mins): ";                              
-		cin.clear();
-		cin.ignore(10000, '\n');
-		cin >> input;
-		if (cin.fail())
-		cout << "invalid input.Please try again." << endl;
-	} while (cin.fail());
-
-	cin.clear();
-	cin.ignore(10000, '\n');
-	return input;
-}
-
-//no duration included
-int inputChecking2() {                                      
+int inputChecking() {
 	int input;
 	do {
 		cin.clear();
@@ -53,9 +33,9 @@ int inputChecking2() {
 
 	cin.clear();
 	cin.ignore(10000, '\n');
-
 	return input;
 }
+
 using namespace chrono;
 int main()
 {
@@ -75,110 +55,72 @@ int main()
 	vector<string> topicNames;
 	vector<int> topicDurations;
 
-	    //get the number of topics to learn
-	    std::cout << "TIMETABLE\n";
-		cout << "Number of topics to learn:";
-		cin >> numofTopics;
-		for (int x = 0; x < numofTopics; x++) {
-			cout << "TOPIC " << x+1 << endl;
-			cout << "Input the topic and the duration for learning that topic.\n";
-			cout << "Topic: ";
-			cin >> topic;
-			topicNames.push_back(topic);
-			duration = inputChecking();
-			topicDurations.push_back(duration);
+	//get the number of topics to learn
+	std::cout << "TIMETABLE\n";
+	cout << "Number of topics to learn:";
+	cin >> numofTopics;
 
+	for (int x = 0; x < numofTopics; x++) {
+		cout << "TOPIC " << x + 1 << endl;
+		cout << "Input the topic and the duration for learning that topic.\n";
+		cout << "Topic: ";
+		cin >> topic;
+		topicNames.push_back(topic);
+
+		cout << "Duration(mins): ";
+		duration = inputChecking();                             //get duration(runs through input checking)
+		topicDurations.push_back(duration);
+
+	}
+
+	cout << endl;
+
+	//display the whole list
+	for (int x = 0; x < numofTopics; x++) {
+		displayTimeTable(topicNames[x], topicDurations[x]);
+	}
+
+	cout << endl;
+
+
+	//shows current subject
+	for (int topics = 0; topics < numofTopics; topics++) {
+		for (int x = 0; x < numofTopics; x++) {                          //display the whole list
+			displayTimeTable(topicNames[x], topicDurations[x]);
 		}
 
 		cout << endl;
-
-		//display the whole list
-		for (int x = 0; x < numofTopics; x++) {                          
-			displayTimeTable(topicNames[x], topicDurations[x]);          
-		}
-
-		cout << endl;
-
-		//check if list is correct
-		/*
-		cout << "is the list and their ordering correct?\n1 = yes, 2 = no\n";
-		int option;
-		for (;;) {                                                      //makes sure the input is either a 1 or a 2
+		cout << "Topic : " << topicNames[topics] << endl;
+		cout << "Duration : " << topicDurations[topics] << "min" << endl;
+		cout << "press 1 to start.";
+		int option = 0;
+		do {
+			cin.clear();
+			cin.ignore(10000, '\n');
 			cin >> option;
-			if (option == 1 || option == 2) {
+		} while (option != 1);
+
+
+		cin.clear();
+		cin.ignore(10000, '\n');
+
+		//timer
+		steady_clock::time_point start = steady_clock::now();
+		steady_clock::time_point end = start + minutes(duration);
+		for (;;) {
+			steady_clock::time_point current = steady_clock::now();
+			if (current == end)
 				break;
-			}
-			else
-			{
-				if (cin.fail()) {
-					cin.clear();
-					cin.ignore(10000, '\n');
-				}
-				cout << "invalid input please try again\n";
-				continue;
-			}
 		}
-		*/
-		/*
-		if (option == 2) {
-			cout << "What would you like to do?\n"
-				<< "1 -> add more subjects\n"
-				<< "2 -> remove subjects\n"
-				<< "3 -> change duration\n"
-				<< "4 -> nothing is wrong\n";
-			
-			option = 0;
-			for (;;) {
-				option = inputChecking();
-				if (option < 1 || option>4)
-					continue;
-				break;
-			}
-		}
-		*/
+		PlaySound(TEXT("C:\\Users\\siong\\source\\repos\\timetable\\timetable\\times_up.wav"), NULL, SND_FILENAME);
 
-		//shows current subject
-		for (int topics = 0; topics < numofTopics ; topics++) {
-			for (int x = 0; x < numofTopics; x++) {                          //display the whole list
-				displayTimeTable(topicNames[x], topicDurations[x]);
-			}
+	}
 
-			cout << endl;
-			cout << "Topic : " << topicNames[topics] << endl;
-			cout << "Duration : " << topicDurations[topics] << "min" << endl;
-			cout << "press 1 to start.";
-			int option = 0;
-			for (;;) {
-				cin >> option;
-				if (option == 1)
-					break;
+	cout << "congratulations, you have finished your entire schedule. :)";
 
-				if (cin.fail()) {
-					cin.clear();
-					cin.ignore(10000, '\n');
-				}
-				cout << "invalid input\n";
-			}
+	cout << ctime(&timenow) << endl;
 
-			
-
-			//timer
-			steady_clock::time_point start = steady_clock::now();
-			steady_clock::time_point end = start + minutes(duration);
-			for (;;) {
-				steady_clock::time_point current = steady_clock::now();
-				if (current == end)
-					break;
-			}
-			PlaySound(TEXT("C:\\Users\\siong\\source\\repos\\timetable\\timetable\\times_up.wav"), NULL, SND_FILENAME);
-
-		}
-
-		cout << "congratulations, you have finished your entire schedule. :)";
-
-		cout << ctime(&timenow) << endl;
-
-		cout << endl;
+	cout << endl;
 }
 
 
